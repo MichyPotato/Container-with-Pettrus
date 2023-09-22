ea0f8e4e0558497ca0a7301e0c2d2017a8391238
MichyPotato <96063090+MichyPotato@users.noreply.github.com>
2023-09-22 19:20:33 +0000

    initialized console shelf, console

---
+++ ConsoleShelf.java
@@ -0,0 +1,14 @@
+
+//package unconsolable.seven;
+
+
+public class ConsoleShelf{
+
+    //fields for the shelf variable def
+    private double height;
+    private int availableSpace;
+    private boolean isFull;
+    private String color;
+    private Console[] consoleArray;
+
+}
