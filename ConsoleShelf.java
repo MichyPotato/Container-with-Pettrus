3e135bdc58ed3583753320ba1edba04feae44fb1
MichyPotato <96063090+MichyPotato@users.noreply.github.com>
2023-09-22 21:39:40 +0000

    full constructor for console shelf

--- ConsoleShelf.java
+++ ConsoleShelf.java
@@ -4,11 +4,19 @@
 
 public class ConsoleShelf{
 
-    //fields for the shelf variable def
+    //fields for the shelf variable def (Michelle Luo)
     private double height;
     private int availableSpace;
     private boolean isFull;
     private String color;
     private Console[] consoleArray;
 
+    //full constructor console shelf (Michelle Luo)
+    public ConsoleShelf(double height, int availableSpace, boolean isFull, String color, Console[] consoleArray){
+        this.height = height;
+        this.availableSpace = availableSpace;
+        this.isFull = isFull;
+        this.color = color;
+        this.consoleArray = consoleArray;
+    }
 }
