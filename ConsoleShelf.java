df4dd9347c1c8dd8d6ddf8549ab4ed7d7565f40b
MichyPotato <96063090+MichyPotato@users.noreply.github.com>
2023-09-24 19:10:39 +0000

    constructors for console shelf

--- ConsoleShelf.java
+++ ConsoleShelf.java
@@ -5,11 +5,29 @@
 public class ConsoleShelf{
 
     //fields for the shelf variable def (Michelle Luo)
-    private double height;
-    private int availableSpace;
-    private boolean isFull;
-    private String color;
-    private Console[] consoleArray;
+    private double height; //in meters
+    private int availableSpace; //total number of console box spaces on shelf
+    private boolean isFull; //is it full?
+    private String color; //color of the shelf
+    private Console[] consoleArray; //information about the consoles on the shelf
+
+    //default constructor console shelf (Michelle Luo)
+    public ConsoleShelf(){
+        this.height = 2.6;
+        this.availableSpace = 8;
+        this.isFull = false;
+        this.color = "gray";
+        this.consoleArray = // INSERT AN ARRAY FULL OF NULL VALUES SOMEHOW;
+    }
+    
+    //partial constructor console shelf (Michelle Luo)
+    public ConsoleShelf(int availableSpace, boolean isFull, Console[] consoleArray){
+        this.height = 2.6;
+        this.availableSpace = availableSpace;
+        this.isFull = isFull;
+        this.color = "gray";
+        this.consoleArray = consoleArray;
+    }
 
     //full constructor console shelf (Michelle Luo)
     public ConsoleShelf(double height, int availableSpace, boolean isFull, String color, Console[] consoleArray){
@@ -18,5 +36,5 @@ public class ConsoleShelf{
         this.isFull = isFull;
         this.color = color;
         this.consoleArray = consoleArray;
-    }
+    }    
 }
