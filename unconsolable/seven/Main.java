022dd53fca04212d4911cda8d64ae59807c044b1
MichyPotato <96063090+MichyPotato@users.noreply.github.com>
2023-09-24 23:29:17 +0000

    main set up for crud portion

--- unconsolable/seven/Main.java
+++ unconsolable/seven/Main.java
@@ -5,11 +5,33 @@
  * Period 7
  */
 package unconsolable.seven;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]){
-        //insert thing here 
-        //r we doing a menu or a one timer
+
+
+
+        //CRUD user input reader (Michelle Luo)
+        Scanner userInputObject = new Scanner(System.in);
+        System.out.println("Choose a CRUD Option for the Container:\n1. Create a new Console to be added to the shelf\n2. Read off all the Consoles on the Shelf\n3. Update a specific Console on the shelf\n4. Delete a Console from the shelf");
+        int userInput = userInputObject.nextInt();
+        switch (userInput) {
+            case 1:
+                //implement ConsoleShelf.create()
+                break;
+            case 2:
+                // implement ConsoleShelf.read()
+                break;
+            case 3:
+                //implement ConsoleShelf.update()
+                break;
+            case 4:
+                //implement ConsoleShelf.delete()
+                break;
+            default:
+                break;
+        }
 
 
     }
